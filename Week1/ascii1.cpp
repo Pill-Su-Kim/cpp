2edb88724a736d2fe0a2f0b2c01eb86e67c54cd3
Woosuk Sung <a01053126089@gmail.comgit config --global user.name Pill-Su-Kim>
2025-03-19 00:58:08 +0900

    homework

---
+++ Week1/ascii1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void drawTree() {
+    cout << "        *        " << endl;
+    cout << "       ***       " << endl;
+    cout << "      *****      " << endl;
+    cout << "     *******     " << endl;
+    cout << "    *********    " << endl;
+    cout << "   ***********   " << endl;
+    cout << "  *************  " << endl;
+    cout << " *************** " << endl;
+    cout << "        ||       " << endl;
+    cout << "        ||       " << endl;
+}
+
+int main() {
+    cout << "ASCII Art: Christmas Tree" << endl;
+    drawTree();
+    return 0;
+}
